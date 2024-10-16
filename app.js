5a2930a054998e9774be3c5d85850e3caf3dd447
robertodelavi <roberto.delavy@gmail.com>
2024-10-16 11:09:55 -0300

    Tentando resolver o BO dos arquivos

--- app.js
+++ app.js
@@ -1,26 +1,36 @@
-require('dotenv/config')
+require('dotenv/config');
 process.env.TZ = 'America/Sao_Paulo'; // Configurar timezone
-const { responseTime } = require('./config/info');
-
+const bodyParser = require('body-parser');
 const express = require('express');
 const cors = require('cors');
-const app = express();
 const routes = require("./routes");
-// const routerReports = require("./reports");
 
-app.use(express.json());
+const app = express();
+
+// Configuração do CORS
 app.use(cors({ origin: '*' }));
+
+// Middleware para analisar requisições JSON e URL-encoded
+app.use(express.json());
+// app.use(bodyParser.json({ limit: '20mb' })); // Aumente conforme necessário
+// app.use(bodyParser.urlencoded({ limit: '20mb', extended: true }));
+
+// Middleware para registrar requisições e tempo de execução
+// app.use((req, res, next) => {
+//     const start = Date.now();
+//     console.log(`${req.method} ${req.url}`);
+
+//     res.on('finish', () => {
+//         const duration = Date.now() - start;
+//         console.log(`Request to ${req.url} took ${duration}ms`);
+//     });
+
+//     next();
+// });
+
+// Rotas
 app.use(routes);
-// app.use(routerReports);
 app.use('/api/uploads', express.static('uploads'));
-app.use(responseTime);
-
-// const fs = require('fs');
-// const axios = require('axios');
-// const FormData = require('form-data');
-
-// const apiToken = process.env.AUTENTIQUE_TOKEN
-// const url = 'https://api.autentique.com.br/v2/graphql';
 
 const port = process.env.PORT ?? 3333;
 
